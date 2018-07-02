b1cb3cde0ad9cb96e573d597a727f1efc5c29e7b
Phazoning <40458828+Phazoning@users.noreply.github.com>
2018-07-02 13:48:04 +0200

    up 0.0.1
    Moved main script values into main function
    Added a third label
    Added try-except load database procedure and system exit full failure code

--- Nikonikonuiii.py
+++ Nikonikonuiii.py
@@ -2,37 +2,52 @@ from tkinter import *
 import mysql.connector
 import system
 
-kappaUser = 0
-kappaPassword = 0
-kappaDatabase = 0
 def intro():
 	Master = Tk()
 	Label(Master, text = "Nombre de Usuario").grid(row=0)
 	Label(Master, text = "Contraseña").grid(row=1)
+	Label(Master, text = "Base de datos".grid(row=2)
 	
 	en1 = Entry(Master)
 	en2 = Entry(Master)
+	e3 = Entry(Master)
 	
 	en1.grid(row=0, column=1)
 	en2.grid(row=1, column=1)
+	en3.grid(row=2, column=1)
 	
-	Button(Master, text = "input", command = data(en1, en2, Master).grid(row=4, column=0, sticky=W, pady=4)
+	Button(Master, text = "input", command = data(en1, en2, en3).grid(row=4, column=0, sticky=W, pady=4)
 	Button(Master, text = "exit", command = sys.exit).grid(row=4, column=0, sticky=W, pady=4)
-	
-	Master = Tk()
-	Label(Master, text 
+	       
+	kappaUser = 0
+	kappaPassword = 0
+	kappaDatabase = 0
+	base = 0
 	
 	mainloop()
 	
-def data(input1, input2):
+def data(i1, i2, i3):
 	check = False
+	Essit = 0
 	while check == False:
 		try:
-			kappaDatabase = mysql.connector.connect(host="localhost", user=str(en1), password=str(en2))
+			lambdaDatabase = mysql.connector.connect(host="localhost", user=str(i1), password=str(i2), database = str(i3))
 			kappaUser, kappaPassword = en1, en
 			check = True
 		except:
-			print "No se ha podido acceder, nombre de usuario o contraseña incorrectos"
+			try:
+	       			Database = tkSimpleDialog.askstring("No database", "Enter name to create a new one", Parent=Master)
+	       			lambdaDatabase = mysql.connector.connect(host="localhost", user=str(en1), password=str(en2))
+	       			cursor= lambdaDatabase.cursor()
+	       			cursor.execute("CREATE DATABASE " + str(Database))
+	       			kappaDatabase = Database
+	       			base = lambdaDatabase)
+	       		except:
+	      			if Essit == 10:
+	      				tkMessageBox.FunctionName("No way", "No attempts left, closing...", parent = Master)
+	      				sys.close()
+	      			else:
+	      				Essit += 1
 	
 
 
